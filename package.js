f779e47e3ea19d15007b1245fa6cea2b7fcfbcab
Emmanuel Prochasson <eprochasson@gmail.com>
2013-10-16 11:43:24 +0800

    Add jQuery+Less dependency, bump version number

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
-summary: "Bootstrap 3, with Less files."
+    summary: "Bootstrap 3, with Less files."
 });
 
 Package.on_use(function (api, where) {
+    api.use('jquery', 'client');
+    api.use('less','client');
     api.add_files('lib/js/bootstrap.js', 'client');
 });
