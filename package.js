104425bf4d905cdb0e56154ac13d70558c775132
Marco Pfeiffer <cheat2000-git@yahoo.de>
2014-02-19 00:50:16 +0100

    update to 3.1.1

--- package.js
+++ package.js
@@ -5,7 +5,18 @@ Package.describe({
 Package.on_use(function (api) {
     api.use('jquery', 'client');
     api.use('less','client');
-    api.add_files('lib/js/bootstrap.js', 'client');
+    api.add_files('lib/js/affix.js', 'client');
+    api.add_files('lib/js/alert.js', 'client');
+    api.add_files('lib/js/button.js', 'client');
+    api.add_files('lib/js/carousel.js', 'client');
+    api.add_files('lib/js/collapse.js', 'client');
+    api.add_files('lib/js/dropdown.js', 'client');
+    api.add_files('lib/js/modal.js', 'client');
+    api.add_files('lib/js/popover.js', 'client');
+    api.add_files('lib/js/scrollspy.js', 'client');
+    api.add_files('lib/js/tab.js', 'client');
+    api.add_files('lib/js/tooltip.js', 'client');
+    api.add_files('lib/js/transition.js', 'client');
 
     // fonts
     api.add_files('lib/fonts/glyphicons-halflings-regular.eot', 'client');
