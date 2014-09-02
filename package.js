31dea8f4ac3ff98780cd59308cb5a52e4cc8110e
Marco Pfeiffer <m4pfeiffer+github@gmail.com>
2014-09-02 09:07:09 +0200

    fixed colon

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "nemo64:bootstrap",
   summary: "Highly configurable bootstrap integration.",
-  version: "3.2.0",
+  version: "3.2.0-1",
   git: "https://github.com/Nemo64/meteor-bootstrap"
 });
 
